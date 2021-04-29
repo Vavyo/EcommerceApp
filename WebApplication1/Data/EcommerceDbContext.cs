53f6a9f361b51cf4fe2771e08ccd19c2429303d4
Vavyo <vavyo2121@gmail.com>
2021-04-29 11:27:24 -0500

    Fleshed out DbContext

--- WebApplication1/Data/EcommerceDbContext.cs
+++ WebApplication1/Data/EcommerceDbContext.cs
@@ -1,13 +1,22 @@
 ﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using WebApplication1.Models;
 using WebApplication1.Models.Identity;
 
 namespace WebApplication1.Data
 {
     public class EcommerceDbContext : IdentityDbContext<ApplicationUser>
     {
+        public EcommerceDbContext(DbContextOptions options) : base(options)
+        {
+
+        }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<ProductCategory> ProductCategories { get; set; }
     }
 }
