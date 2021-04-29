13fc944f8b9c5aae451887feedca257d0b08ed2d
Vavyo <vavyo2121@gmail.com>
2021-04-29 11:23:14 -0500

    dbcontext inherits identity

--- WebApplication1/Data/EcommerceDbContext.cs
+++ WebApplication1/Data/EcommerceDbContext.cs
@@ -3,10 +3,11 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using WebApplication1.Models.Identity;
 
 namespace WebApplication1.Data
 {
-    public class EcommerceDbContext : IdentityDbContext
+    public class EcommerceDbContext : IdentityDbContext<ApplicationUser>
     {
     }
 }
