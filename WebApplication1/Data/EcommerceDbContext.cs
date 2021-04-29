95ac27a5687a83ad8db74aa885417bc9f9e5d2dd
Vavyo <vavyo2121@gmail.com>
2021-04-29 10:30:41 -0500

    DbContext and added Identity package

---
+++ WebApplication1/Data/EcommerceDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication1.Data
+{
+    public class EcommerceDbContext : IdentityDbContext
+    {
+    }
+}
