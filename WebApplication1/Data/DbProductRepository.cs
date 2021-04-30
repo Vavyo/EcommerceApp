099c8701ce9b73805fe32496d27e6f6e86415529
Vavyo <vavyo2121@gmail.com>
2021-04-29 19:19:04 -0500

    product Repo Create

---
+++ WebApplication1/Data/DbProductRepository.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApplication1.Data.Interfaces;
+using WebApplication1.Models.Api;
+
+namespace WebApplication1.Data
+{
+    public class DbProductRepository : IProductRepository
+    {
+        private EcommerceDbContext context;
+
+        public DbProductRepository(EcommerceDbContext context)
+        {
+            this.context = context;
+        }
+        public Task CreateProduct(ProductDto product)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<ProductDto>> GetAllProducts()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ProductDto> GetProduct(int productId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
